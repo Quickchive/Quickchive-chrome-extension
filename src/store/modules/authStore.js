97b751025b6a237cf7d35a7f4541245abcc32be1
oseunghyun <ohsh1477@gmail.com>
2022-09-18 23:13:03 +0900

    [feat] 프로필 조회 api 함수 추가

--- src/store/modules/authStore.js
+++ src/store/modules/authStore.js
@@ -1,39 +1,34 @@
-import { getAuthFromCookie } from "../../utils/cookies";
+import { fetchProfile } from "../../api/user";
 
 const authStore = {
-  // namespaced: true,
   state: {
-    nickname: "",
-    email: "",
-    loginState: false || localStorage.getItem("refreshToken"),
-    oauthLoginState: false,
-    accessToken: "" || getAuthFromCookie("accessToken"),
-    refreshToken: "" || localStorage.getItem("refreshToken"),
-    oauthInfo: false || localStorage.getItem("oauthInfo"),
-    oauthName: "" || localStorage.getItem("oauthInfo"),
-    stayLoginState: false || localStorage.getItem("stayLogin"),
+    loginState: false,
   },
   getters: {
     // 로그인 여부 확인
     isLogin(state) {
       return state.loginState;
     },
-    // 소셜 로그인 여부 확인
-    isOauthLogin(state) {
-      return state.oauthLoginState;
-    },
-    isUserStayLogin(state) {
-      return state.stayLoginState;
-    },
-    // 소셜 정보 가져옴
-    getOauthName(state) {
-      return state.oauthName;
-    },
-    getNickname(state) {
-      return state.nickname;
+  },
+  actions: {
+    // 프로필 조회이자 로그인 여부 확인
+    async FETCH_PROFILE({ commit }) {
+      try {
+        const { data } = await fetchProfile();
+        console.log("vuex에서 로그인 여부 확인중", data);
+        if (data.statusCode == 201) {
+          commit("setLoginState", true);
+        } else if (data.statusCode == 200) {
+          commit("setLoginState", true);
+        }
+      } catch (error) {
+        console.log(error);
+        if (error.statusCode == 401) {
+          commit("setLoginState", false);
+        }
+      }
     },
   },
-  actions: {},
 };
 
 export default authStore;
