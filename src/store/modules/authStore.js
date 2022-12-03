45c50737515a5642832263964550389cbb212d4d
oseunghyun <ohsh1477@gmail.com>
2022-12-03 19:13:08 +0900

    feat: 로그인 여부 뮤테이션 추가, 토큰 갱신 기능 추가, 로그인 기본 상태값 true로 변경 -#7

--- src/store/modules/authStore.js
+++ src/store/modules/authStore.js
@@ -1,8 +1,8 @@
-import { fetchProfile } from "../../api/user";
+import { fetchProfile } from '../../api/user';
 
 const authStore = {
   state: {
-    loginState: false,
+    loginState: true,
   },
   getters: {
     // 로그인 여부 확인
@@ -10,23 +10,36 @@ const authStore = {
       return state.loginState;
     },
   },
+  mutations: {
+    setLoginState(state, loginState) {
+      state.loginState = loginState;
+    },
+  },
   actions: {
     // 프로필 조회이자 로그인 여부 확인
     async FETCH_PROFILE({ commit }) {
       try {
         const { data } = await fetchProfile();
-        console.log("vuex에서 로그인 여부 확인중", data);
+        console.log('vuex에서 로그인 여부 확인중', data);
         if (data.statusCode == 201) {
-          commit("setLoginState", true);
+          commit('setLoginState', true);
         } else if (data.statusCode == 200) {
-          commit("setLoginState", true);
+          commit('setLoginState', true);
         }
       } catch (error) {
         console.log(error);
         if (error.statusCode == 401) {
-          commit("setLoginState", false);
+          commit('setLoginState', false);
         }
       }
+    },
+    // 액세스 토큰 갱신
+    RENEW_TOKEN(accessToken) {
+      localStorage.setItem('accessToken', accessToken);
+    },
+    // 리프레시 토큰 갱신
+    RENEW_REFRESH_TOKEN(refreshToken) {
+      localStorage.setItem('refreshToken', refreshToken);
     },
   },
 };
