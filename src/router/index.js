eafe8a884c4f5bf788e2baa4c3ed8ad82311d252
oseunghyun <ohsh1477@gmail.com>
2022-12-04 10:52:57 +0900

    feat(router): 로그인 여부를 스토어에서 확인 후 페이지 이동시키도록 구현 - #7

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,47 @@
-import { createWebHashHistory, createRouter } from "vue-router";
-import { store } from "../store/index";
+import { createWebHashHistory, createRouter } from 'vue-router';
+import { store } from '../store/index';
 
 // 라우터 정의
 const routes = [
   // 루트: 랜딩 페이지
   {
-    path: "/",
-    component: () => import("../components/OnboardingComponent.vue"),
-    // beforeEnter: (to, from, next) => {
-    //   if (store.getters.isLogin || store.getters.isOauthLogin) {
-    //     alert("경고");
-    //     next("/main");
-    //   } else {
-    //     next();
-    //   }
-    // },
+    path: '/',
+    component: () => import('../components/OnboardingComponent.vue'),
     beforeEnter: (to, from, next) => {
-      if (localStorage.getItem("accessToken")) {
-        next("/save");
-        console.log("로그인 함");
+      if (store.getters.isLogin) {
+        next('/save');
+        console.log('로그인 함');
       } else {
         next();
-        console.log("로그인 안 했음");
+        console.log('로그인 안 했음');
       }
     },
   },
   {
-    path: "/save",
-    component: () => import("../components/MainComponent.vue"),
+    path: '/save',
+    component: () => import('../components/MainComponent.vue'),
+    beforeEnter: (to, from, next) => {
+      if (store.getters.isLogin) {
+        next();
+        console.log('로그인 함');
+      } else {
+        next('/');
+        console.log('로그인 안 했음');
+      }
+    },
   },
   {
-    path: "/load",
-    component: () => import("../components/CategoryComponent.vue"),
+    path: '/load',
+    component: () => import('../components/CategoryComponent.vue'),
+    beforeEnter: (to, from, next) => {
+      if (store.getters.isLogin) {
+        next();
+        console.log('로그인 함');
+      } else {
+        next('/');
+        console.log('로그인 안 했음');
+      }
+    },
   },
 ];
 
